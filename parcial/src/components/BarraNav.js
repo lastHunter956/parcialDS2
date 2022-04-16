6809b3338c34fe5ed70a1fc4336fcc3da30c9c96
Diego Quintana <dquintana@utb.edu.co>
2022-04-16 15:37:04 -0500

    Barra nav lista, me toco mover las carpetas de iconos y images a src

---
+++ parcial/src/components/BarraNav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container} from "react-bootstrap";
+import "../App.css"
+import utb from "../iconos/utb-logotipo-blanco.png";
+
+export default function BarraNav() {
+  return (
+    <div>
+      <Navbar className="nav-bar-utb" variant="dark">
+        <Container>
+          <Navbar.Brand href="#"><img src={utb} id="logo-utb" /></Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#" id="color-text" >Inicio</Nav.Link>
+            <Nav.Link href="#" id="color-text">Proyectos</Nav.Link>
+            <Nav.Link href="#" id="color-text">Propuestas</Nav.Link>
+            <Nav.Link href="#" id="color-text">Donaciones</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
