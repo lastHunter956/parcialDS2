2f1acd299e57aee82ecaebc4b686454711fd7903
Diego Quintana <dquintana@utb.edu.co>
2022-04-17 20:13:31 -0500

    Separe correctamente los css de los componentes

--- parcial/src/components/BarraNav.js
+++ parcial/src/components/BarraNav.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Container} from "react-bootstrap";
-// import "../App.css"
-import styles from "./BarraNav.module.css";
+import {Link} from "react-router-dom";
+import "./BarraNav.css";
 import utb from "../iconos/utb-logotipo-blanco.png";
 
 export default function BarraNav() {
   return (
     <div>
-      <Navbar className="nav-bar-utb" variant="dark">
+      <Navbar className="principal" variant="dark">
         <Container>
           <Navbar.Brand href="#"><img src={utb} id="logo-utb" /></Navbar.Brand>
           <Nav>
-            <Nav.Link href="#" id="color-text" >Inicio</Nav.Link>
+            <Nav.Link href="#" id="color-text">Inicio</Nav.Link>
             <Nav.Link href="#" id="color-text">Proyectos</Nav.Link>
             <Nav.Link href="#" id="color-text">Propuestas</Nav.Link>
             <Nav.Link href="#" id="color-text">Donaciones</Nav.Link>
