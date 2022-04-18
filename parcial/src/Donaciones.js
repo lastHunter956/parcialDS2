7195f70cde2232998ea57763277ba4ac13bed3d8
lastHunter956 <98895444+lastHunter956@users.noreply.github.com>
2022-04-17 22:08:03 -0500

    adelanto de donaciones

--- parcial/src/Donaciones.js
+++ parcial/src/Donaciones.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Personas from './imagenes/img_center_donacion.png';
+import './Donaciones.css';
+import { Container } from 'react-bootstrap';
 export default function Donaciones() {
   return (
     <div>
-        <h1>Donaciones</h1>
+        <Fragment>
+        <img src={Personas} class="img-fluid" ></img>
+         <Container>
+         <div className="primer_texto">
+          Pellentesque id. Amet nulla facilisi morbi tempus iaculis urna. Placerat in 
+          egestas erat imperdiet sed euismod nisi. Faucibus nisl tincidunt eget nullam 
+          non. Duis at consectetur lorem donec massa sapien faucibus. Diam donec 
+          adipiscing tristique risus nec feugiat in. Aliquet lectus proin nibh nisl 
+          condimentum id. Et netus et malesuada fames ac turpis. Donec massa sapien 
+          faucibus et molestie ac feugiat sed. Risus pretium quam vulputate dignissim.
+          
+          Tristique risus nec feugiat in fermentum posuere urna nec. Ultrices tincidunt 
+          arcu non sodales neque sodales. Fermentum odio eu feugiat pretium nibh 
+          ipsum consequat nisl vel.
+          </div>
+         </Container> 
+         
+        </Fragment>
+
     </div>
   )
 }
