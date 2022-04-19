6bb2246f5689fd946b1d4cc23ad407651551d4bd
lastHunter956 <98895444+lastHunter956@users.noreply.github.com>
2022-04-19 16:25:46 -0500

    d8f867d

--- parcial/src/components/checkoutForm/Controlado.jsx
+++ parcial/src/components/checkoutForm/Controlado.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Controlado() {
+function Controlado() {
+  return (
+    <div>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+  )
+}
+export default Controlado;
+
+function lol() {
   return (
     <div>
       <select class="form-select" aria-label="Default select example">
