932a91e150a7db40c511432a238b9cfcac4a268d
Diego Quintana <dquintana@utb.edu.co>
2022-04-23 00:14:27 -0500

    En caso de que no funcione, este commit lo pueden borrar

---
+++ parcial/src/Proyectos.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Proyectos.css';
+
+export default function Proyectos() {
+  return (
+    <div id="contenido-proyectos">
+        <div id="div-86px"></div>
+        <h1>Proyectos</h1>
+        <ul>
+            <li>Proyecto 1</li>
+            <li>Proyecto 2</li>
+            <li>Proyecto 3</li>
+        </ul>
+    </div>
+  )
+}
