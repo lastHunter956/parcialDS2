6acd6d269aca011af6a1180391beab37a9953f17
lastHunter956 <98895444+lastHunter956@users.noreply.github.com>
2022-04-23 22:23:28 -0500

    conectar con base de datos

---
+++ parcial/src/mysql.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const conection = mysql.createConnection({//conexion a la base de datos
+    host: 'localhost',//direccion del servidor
+    user: 'root',//usuario de la base de datos
+    password: '',//contraseña de la base de datos
+    database: 'parcial'//nombre de la base de datos a utilizar
+});
+
+conection.connect( (err) => {//err es una variable que se puede usar para verificar si hay un error
+    if (err) throw err
+        console.log('Conexion a la base de datos establecida')
+})
+const insertar = "INSERT INTO adminitrador (nombre, apellido, correo, celular, clave) VALUES ('lucas', 'garcia', 'lgarcia@utb.edu.co', '3052204282', '157')";
+conection.query(insertar, (err, result) => {
+    if (err) throw err
+        console.log('Datos insertados')
+})
+conection.query('SELECT * FROM adminitrador', (err, result) => {//consulta a la base de datos
+    if (err) throw err
+        console.log(`Los datos en la base de dato son: `)
+        console.log(result)//result es un array con los datos 
+        console.log(`La cantidad de datos es: `, result.length)//result.length es la cantidad de datos en el array
+        console.log(`la primera persona es: ${result[0].nombre} y su codigo ${result[0].id_administrador} `)//result[0].nombre es el nombre de la persona
+})  // SELECT * FROM usuarios
+
+conection.end()
