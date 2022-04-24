5b214025f173f68a883b6561bc2f2c2c0466342c
lastHunter956 <98895444+lastHunter956@users.noreply.github.com>
2022-04-24 14:16:26 -0500

    crando las varibles de entorno

--- parcial/src/Seccion.js
+++ parcial/src/Seccion.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//invocamos a dotenv para que nos cargue las variables de entorno
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env' });
+
+//directorio publico
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname+'/public'));
+//establecemos el motor de plantillas
+app.set('view engine', 'ejs');
+//invocamos a bcryptjs para encriptar las contraseñas
+const bcryptjs = require('bcryptjs');
+//variables de seccion
+const session = require('express-session');
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
 app.listen(3000, (req, res) => {
     console.log('Servidor corriendo en el puerto 3000');
 });
+
+
